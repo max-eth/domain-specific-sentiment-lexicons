5fd0b0cb32b444dffd050a7dab692d87de930683
Oliver Rausch <oliverrausch99@gmail.com>
2020-04-14 18:04:23 +0200

    changes

--- socialsent/representations/makelowdim.py
+++ socialsent/representations/makelowdim.py
@@ -8,6 +8,7 @@ def run(index_path, in_file, out_path, dim=300, keep_words=None):
         base_embed = Explicit.load(in_file, index_path, normalize=False)
         if keep_words != None:
             base_embed = base_embed.get_subembed(keep_words)
+        print("Factorising {} with shape {}".format(base_embed.m.nnz, base_embed.m.shape))
         u, s, v = randomized_svd(base_embed.m, n_components=dim, n_iter=5)
         np.save(out_path + "-u.npy", u)
         np.save(out_path + "-v.npy", v)

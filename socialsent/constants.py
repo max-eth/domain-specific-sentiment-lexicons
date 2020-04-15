6faeec21ab5befea153ccb2974d69dfd29bfec33
Anton Schaefer <scanton@mit.edu>
2020-04-15 14:02:26 +0200

    corrected constants

--- socialsent/constants.py
+++ socialsent/constants.py
@@ -1,15 +1,29 @@
 import os
 
+scores_interval = [None,0]
+scores = '' if scores_interval is None else 'scores-{}-'.format(scores_interval[1] if scores_interval[1] is not None else 'top')
 BASE_DIR = ''
 DATA_DIR = os.path.join(BASE_DIR, 'data')
-DICTS = 'dict.pkl'
 OUTPUTS = 'outputs.json'
-INDICES = 'index.pkl'
-COUNTS = 'counts.bin'
-PPMI = 'ppmi.bin'
-PPMI_INDEX = 'ppmi-index.pkl'
-VECS = 'vecs'
-POLARITIES = 'polarities.pkl'
+DICTS = scores + 'dict.pkl'
+INDICES = scores + 'index.pkl'
+COUNTS = scores + 'counts.bin'
+PPMI = scores + 'ppmi.bin'
+PPMI_INDEX = scores + 'ppmi-index.pkl'
+VECS = scores + 'vecs'
+POLARITIES = scores + 'polarities.pkl'
 
-NO_ABOVE = 0.1
+def filter_comments(x):
+    if scores_interval is not None:
+        if scores_interval[0] is not None:
+            if x['score'] <= scores_interval:
+                return False
+        if scores_interval[1] is not None:
+            if x['score'] > scores_interval:
+                return False
+    return True
+
+
+
+NO_ABOVE = 0.4
 NO_BELOW = 100
